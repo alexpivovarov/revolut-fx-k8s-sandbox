6953e0e6db1d0db8ca5b2be2b8234ff5104d08b5
Alex Pivovarov <139866046+alexpivovarov@users.noreply.github.com>
2025-08-29 17:26:56 +0300

    Create build.gradle.kts

---
+++ gateway/build.gradle.kts
@@ -0,0 +1,26 @@
+plugins {
+    id("org.springframework.boot")
+    id("io.spring.dependency-management")
+    id("java")
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("org.springframework.cloud:spring-cloud-starter-gateway")
+    runtimeOnly("io.micrometer:micrometer-registry-prometheus")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+
+    //tracing libraries
+    implementation("io.micrometer:micrometer-tracing-bridge-otel")
+    implementation("io.opentelemetry:opentelemetry-exporter-otlp")
+}
+
+dependencyManagement {
+    imports {
+        mavenBom("org.springframework.cloud:spring-cloud-dependencies:2023.0.3")
+    }
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
